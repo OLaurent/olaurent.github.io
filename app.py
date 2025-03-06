12c83b9b6342b739d884c8c52fbc7b9e0885822b
Olivier Laurent <olivier.laurent@gmail.com>
2025-03-06 23:26:57 +0100

    update app with test exercices

--- app.py
+++ app.py
@@ -32,9 +32,56 @@ todos = [
     }
 ]
 
+
+
+exercices = [
+    {
+        "id": str(uuid.uuid4()),
+        "level": "Première",
+        "theme": "Suites numériques",
+        "selected" : False, 
+        "content": r"""
+        <p>Soit $(u_n)$ la suite définie pour $n \in \mathbb{N}$ par $u_n=-8 \times 0,9^n + 15$.</p>
+        <ol>
+            <li>Montrer que la suite $(u_n)$ est croissante.</li>
+            <li>À l'aide de la calculatrice, déterminer le rang $n$ à partir duquel $u_n>14,5$.</li>
+        </ol>
+        """,
+        "latexCode": r"""
+                Soit $(u_n)$ la suite définie pour $n \in \N$ par $u_n=-8\times0,9^n+15$.
+        \begin{enumerate}
+        \item Montrer que la suite $(u_n)$ est croissante.
+        \item \`A l'aide de la calculatrice, déterminer le rang $n$ à partir duquel $u_n>14,5$.
+        \end{enumerate}"
+        """
+    },
+    {
+        "id": str(uuid.uuid4()),
+        "level": "Première",
+        "theme": "Suites numériques",
+        "selected" : True, 
+        "content": r"""
+        <p>Soit $(v_n)$ la suite définie pour $n \in \mathbb{N}$ par $v_n=n^2-11n+10$.</p>
+        <ol>
+            <li>Montrer que $v_{n+1}-v_n=2n-10$.</li>
+            <li>Résoudre l'inéquation $2n-10\geqslant 0$ et en déduire que la suite $(v_n)$ est monotone à partir d'un certain rang que l'on précisera.</li>
+        </ol>
+        """,
+        "latexCode": r"""
+        Soit $(v_n)$ la suite définie pour $n \in \N$ par $v_n=n^2-11n+10$.
+        \begin{enumerate}
+            \item Montrer que $v_{n+1}-v_n=2n-10$.
+            \item Résoudre l'inéquation $2n-10\geqslant 0$ et en déduire que la suite $(v_n)$ est monotone à partir d'un certain rang que l'on précisera.
+        \end{enumerate}
+        """
+    }
+]
+
+
+
 @app.route('/')
 def home():
-    return render_template('index.html', todos=todos)
+    return render_template('index.html', todos=todos, exercices=exercices)
 
 @app.route('/create_todo', methods=['POST'])
 def create_todo():
@@ -60,6 +107,14 @@ def delete_todo(id):
     todos = [todo for todo in todos if todo['id'] != id]
     return redirect('/')
 
+
+@app.route('/filter_exercices', methods=['GET'])
+def filter_exercices():
+    level = request.args.get('level')
+    if level == 'all':
+        return render_template('index.html', todos=todos, exercices=exercices)
+    filtered_exercices = [exercice for exercice in exercices if exercice['level'] == level]
+    return render_template('index.html', todos=todos, exercices=filtered_exercices)
 
 if __name__ == '__main__':
     app.run(debug=True)
