845002322baf3693026539992ca306bd93634b7d
Olivier Laurent <olivier-xavier.laurent@ac-toulouse.fr>
2025-03-07 18:41:22 +0000

    ajout de fichiers de typage pour plusieurs bibliothèques et mise à jour des versions

--- site/src/app.py
+++ site/src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
+from docx import Document
 
 app = Flask(__name__)
 app.debug = True
@@ -58,6 +60,60 @@ def select_exercise():
     print('Exercices sélectionnés:', selected_exercises)
     return redirect(url_for('home'))
 
+
+@app.route('/export_menu')
+def export_menu():
+    selected_exercises = session.get('selected_exercises', [])
+    if not selected_exercises:
+        return "Aucun exercice sélectionné", 400
+
+    exercices = Exercice.query.filter(Exercice.id.in_(selected_exercises)).all()
+    return render_template('export.html', exercices=exercices)
+
+@app.route('/export_exercises', methods=['GET'])
+def export_exercises():
+    selected_exercises = session.get('selected_exercises', [])
+    if not selected_exercises:
+        return "Aucun exercice sélectionné", 400
+
+    exercices = Exercice.query.filter(Exercice.id.in_(selected_exercises)).all()
+    exercices_data = [
+        {
+            'id': exercice.id,
+            'level': exercice.level,
+            'theme': exercice.theme,
+            'content': exercice.content,
+            'latex_code': exercice.latex_code,
+            'correction': exercice.correction,
+            'latex_correction': exercice.latex_correction
+        }
+        for exercice in exercices
+    ]
+
+    export_format = request.args.get('format', 'json')
+    if export_format == 'tex':
+        export_file_path = '/tmp/selected_exercises.tex'
+        with open(export_file_path, 'w') as export_file:
+            for exercice in exercices_data:
+                export_file.write(f"\\section*{{{exercice['level']} - {exercice['theme']}}}\n")
+                export_file.write(f"{exercice['content']}\n\n")
+                export_file.write(f"\\subsection*{{Correction}}\n")
+                export_file.write(f"{exercice['correction']}\n\n")
+    elif export_format == 'docx':
+        export_file_path = '/tmp/selected_exercises.docx'
+        document = Document()
+        for exercice in exercices_data:
+            document.add_heading(f"{exercice['level']} - {exercice['theme']}", level=1)
+            document.add_paragraph(exercice['content'])
+            document.add_heading("Correction", level=2)
+            document.add_paragraph(exercice['correction'])
+        document.save(export_file_path)
+    else:
+        export_file_path = '/tmp/selected_exercises.json'
+        with open(export_file_path, 'w') as export_file:
+            json.dump(exercices_data, export_file)
+
+    return send_file(export_file_path, as_attachment=True, download_name=f'selected_exercises.{export_format}')
 
 if __name__ == '__main__':
     app.run(debug=True)
